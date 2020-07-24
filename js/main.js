595bf9b9d58cc5b6e29bca3fd3baff9ebd1e44ad
kwarchal <kamilwarchal295@gmail.com>
2020-07-24 11:34:40 +0200

    sprawdzenie liczby w przedziale

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let liczba = 51;
+
+function main(liczba){
+    if(liczba >= 100 && liczba <= 200){
+        console.log("Liczba znajduje się w przedziale");
+    }else{
+        console.log("Liczba nie znajduje sie w przedziale");
+    }
+}
+
+console.log(main(liczba));
